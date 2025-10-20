91c2196b5d9f158e6263d4ee43a44f8249a5ac86
vivek-gamit <vgamit637@gmail.com>
2025-10-20 07:44:18 +0530

    Bubbling : gose back from target to top

--- bubbling.js
+++ bubbling.js
@@ -3,3 +3,14 @@ let ul = document.querySelector("ul")
 ul.addEventListener("click",(dets)=>{
     dets.target.classList.toggle("lt")
 })
+
+let div = document.querySelector("div")
+let btn = document.querySelector("button")
+
+div.addEventListener("click",()=>{
+    alert("div is activated")
+});
+
+btn.addEventListener("click",()=>{
+    alert("Butten clicked")
+});
