51353aae1e4bb370e2f11dd59b8bc26b5d0f1e9e
vivek-gamit <vgamit637@gmail.com>
2025-10-26 09:49:23 +0530

    element which hind automaticaly after 10 secound

---
+++ autohide.js
@@ -0,0 +1,12 @@
+let hide = document.querySelector("#hide");
+let seconds = 10;
+
+let countdown = setInterval(function(){
+    if(seconds > 0){
+        hide.textContent = seconds;
+        seconds--;
+    }else{
+        clearInterval(countdown);
+        hide.style.display = "none"
+    }
+},1000)
